c79a1985d6606f799efee4f154c29fe4f97dd6ce
Sithmi Rathnayake <sithmisankalya379@gmail.com>
2026-03-02 14:20:46 +0530

    Add search functionality using computed property

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import type { Product, ProductResponse } from './types/product'
 import ProductCard from './components/ProductCard.vue'
+
+
+
 const products = ref<Product[]>([])
 const loading = ref<boolean>(true)
+const searchQuery = ref<string>('')
 
 const fetchProducts = async () => {
   try {
+    
     const response = await fetch('https://dummyjson.com/products')
     const data: ProductResponse = await response.json()
     products.value = data.products
@@ -16,6 +21,11 @@ const fetchProducts = async () => {
     loading.value = false
   }
 }
+const filteredProducts = computed(() => {
+  return products.value.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
 
 onMounted(() => {
   fetchProducts()
@@ -27,6 +37,14 @@ onMounted(() => {
     <h1 class="text-3xl font-bold text-center mb-8">
       EC3404 Mini Project - Product Store
     </h1>
+    <div class="mb-6 flex justify-center">
+  <input
+    v-model="searchQuery"
+    type="text"
+    placeholder="Search products..."
+    class="w-full max-w-md p-2 border rounded-lg shadow-sm"
+  />
+</div>
 
     <div v-if="loading" class="text-center text-lg">
       Loading products...
@@ -37,7 +55,7 @@ onMounted(() => {
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
     >
       <ProductCard
-  v-for="product in products"
+  v-for="product in filteredProducts"
   :key="product.id"
   :product="product"
 />
